df63c9016f7ed298c5ee0a56d155b2fe12b32f3f
selloutdesign <j.selloutdesign@gmail.com>
2016-12-07 09:33:18 -0800

    services fix

--- public/js/controllers/GeekCtrl.js
+++ public/js/controllers/GeekCtrl.js
@@ -1,18 +1,18 @@
 angular.module('GeekCtrl', [])
-.controller('GeekController', [ 'NeighborService', 'CountryService', 'CityService', '$routeParams', '$scope', function( NeighborService, CountryService, CityService, $routeParams, $scope ) {
-	CountryService($routeParams.countryCode).then(function(data) {
+.controller('GeekController', [ 'GeekService', '$routeParams', '$scope', function( GeekService, $routeParams, $scope ) {
+	GeekService.getCountry($routeParams.countryCode).then(function(data) {
 		$scope.countryData = data.geonames.country;
 		$scope.country = $scope.countryData.countryName;
 		$scope.area = $scope.countryData.areaInSqKm;
 		$scope.pop = $scope.countryData.population;
 	});
-	CityService($routeParams.countryCode, $routeParams.city).then(function(data) {
+	GeekService.getCity($routeParams.countryCode, $routeParams.city).then(function(data) {
 		$scope.cityData = data.geonames[0];
 		$scope.city = $scope.cityData.toponymName;
 		$scope.popCap = $scope.cityData.population;
 
 	});
-	NeighborService($routeParams.countryCode).then(function(data) {
+	GeekService.getNeighbors($routeParams.countryCode).then(function(data) {
 		$scope.neighbordata = data.geonames.geoname;
 		console.log(data.geonames);
 		var count = Object.keys(data.geonames.geoname).length;
@@ -20,28 +20,26 @@ angular.module('GeekCtrl', [])
 	});
 	$scope.country = $routeParams.countryCode;
 }])
-.factory('CityService', ['$http', '$q', function($http, $q) {
-	return function(countryCode, city){
-		var tcity =city.trim().replace(/ /g, '%20');
-		return $http.get("http://api.geonames.org/searchJSON?name_equals=" + tcity + "&country=" +countryCode+"&maxRows=1&username=jsells")
-		.then(function(res) {
-			return $q.when(res.data);
-		});
-	};
-}])
-.factory('CountryService', ['$http', '$q', function($http, $q) {
-	return function(countryCode){
-		return $http.get("http://api.geonames.org/countryInfo?country="+countryCode+"&username=jsells")
-		.then(function(res) {
-			return $q.when(res.data);
-		});
-	};
-}])
-.factory('NeighborService', ['$http', '$q', function($http, $q) {
-	return function(countryCode){
-		return $http.get("http://api.geonames.org/neighbours?country="+countryCode+"&username=jsells")
-		.then(function(res) {
-			return $q.when(res.data);
-		});
+.factory('GeekService', ['$http', '$q', function($http, $q) {
+	return{
+	 	getCity: function( countryCode, city){
+			var tcity =city.trim().replace(/ /g, '%20');
+			return $http.get("http://api.geonames.org/searchJSON?name_equals=" + tcity + "&country=" +countryCode+"&maxRows=1&username=jsells")
+			.then(function(res) {
+				return $q.when(res.data);
+			});
+		},
+		getCountry: function(countryCode){
+			return $http.get("http://api.geonames.org/countryInfo?country="+countryCode+"&username=jsells")
+			.then(function(res) {
+				return $q.when(res.data);
+			});
+		},
+		getNeighbors: function(countryCode){
+			return $http.get("http://api.geonames.org/neighbours?country="+countryCode+"&username=jsells")
+			.then(function(res) {
+				return $q.when(res.data);
+			});
+		}
 	};
 }]);
